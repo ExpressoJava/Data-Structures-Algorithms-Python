a56522fa332a2b9ebbeb97372259da3f7af61efd
Bao Pham <ryanvuepham@gmail.com>
2019-10-12 00:19:54 -0500

    implementation linear-search w/ while loop

--- Algorithms/linearSearch.py
+++ Algorithms/linearSearch.py
@@ -1,5 +1,6 @@
 """
-Linear(Sequential) Search - When data items are stored in a collections such as a list, we can say they have a linear/sequential relationship.
+Linear(Sequential) Search - When data items are stored in a collections such as a list, we can say they have a linear/sequential 
+relationship.
 
 Example: 
 Searching for "Harry Potter" book, from shelf in a poorly lit room how would you go about it?
@@ -24,16 +25,34 @@ pseudo:
 
 3. if the end of list, return False
 
+Note: Can be done with both sorted/unsorted items
+
 """
 
 
-def linearSearch(ls, data):
-    for item in ls:
-        if item is data:
+# def linearSearch(ls, data):
+#     for item in ls:
+#         if item == data:
+#             return True
+
+#     return False
+
+
+# # print(linearSearch([6, 3, 9, 0, 5, 8, 2], 0)) # True
+# print(linearSearch([6, 3, 9, 0, 5, 8, 2], 10))  # False
+
+def sequential_search(alist, item):
+    position = 0
+
+    while position < len(alist):
+        if alist[position] == item:
             return True
+        position += 1  # position = posistion + 1
 
     return False
 
 
-# print(linearSearch([6, 3, 9, 0, 5, 8, 2], 0)) # True
-print(linearSearch([6, 3, 9, 0, 5, 8, 2], 10))  # False
+testList = [1, 3, 33, 9, 19, 20, 56, 12, 0]
+
+print(sequential_search(testList, 3))  # True
+print(sequential_search(testList, 99))  # False
