6deae5ef25fcdb6145cdecaaeaddf2a0b52ec360
Bao Pham <ryanvuepham@gmail.com>
2019-10-14 00:34:23 -0500

    implemenation binary-search using while loop

--- Algorithms/binarySearch.py
+++ Algorithms/binarySearch.py
@@ -1,6 +1,6 @@
 """
 Binary Search - the idea is to keep comparing the element with the middle value.  this way
-witch each search we eliminate one half of the list
+with each search we eliminate one half of the list
 
 Alogrithm:
 1. Keep track of two poinster First and Last, These are incremented or decremented to limit
@@ -20,21 +20,41 @@ Alogrithm:
 # Implementation of recursive binary search
 
 
-def binary_search(alist, item):
-    if not alist:  # lis is empty -- our base case
-        return False
+# def binary_search(alist, item):
+#     if not alist:  # lis is empty -- our base case
+#         return False
 
-    midpoint = len(alist) // 2
-    if alist[midpoint] == item:  # found it!
-        return True
+#     midpoint = len(alist) // 2
+#     if alist[midpoint] == item:  # found it!
+#         return True
 
-    if item < alist[midpoint]:  # item is in the first half of list, if at all
-        return binary_search(alist[:midpoint], item)
+#     if item < alist[midpoint]:  # item is in the first half of list, if at all
+#         return binary_search(alist[:midpoint], item)
 
-    # otherwise item is in the second half of list, if at all
-    return binary_search(alist[midpoint + 1:], item)
+#     # otherwise item is in the second half of list, if at all
+#     return binary_search(alist[midpoint + 1:], item)
 
 
-testlist = [2, 3, 5, 9, 11, 15, 17, 19, 33, 39, 55, 76, 99, 101, 117, 220]
-print(binary_search(testlist, 117))  # True
-print(binary_search(testlist, 300))  # False
+# testlist = [2, 3, 5, 9, 11, 15, 17, 19, 33, 39, 55, 76, 99, 101, 117, 220]
+# print(binary_search(testlist, 117))  # True
+# print(binary_search(testlist, 300))  # False
+
+# Give list B = [2, 5, 7, 8, 9, 11, 15, 17], find if 3 is present in the list or not
+def binary_search(ls, data):
+    first = 0
+    last = len(ls) - 1
+    done = False
+
+    while first <= last and not done:
+        mid = (first + last) // 2  # we use // to get whole number
+        if ls[mid] == data:
+            done = True
+        else:
+            if ls[mid] > data:
+                last = last - 1
+            else:
+                first = first + 1
+    return done
+
+
+print(binary_search([2, 5, 7, 8, 9, 11, 15, 17], 3))  # False
